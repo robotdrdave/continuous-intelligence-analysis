f3dbce1ced8cd2974b37fd9a7a1d4211ef06a65b
David Michel <robotdrdave@Davids-MacBook-Pro-3.local>
2019-06-24 16:18:41 +0100

    now logging metrics

--- src/analysis.py
+++ src/analysis.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-data = pd.read_csv("/Users/robotdrdave/git/continuous-intelligence-workshop/data/raw/store47-2016.csv")
+data = pd.read_csv("/Users/robotdrdave/git/continuous-intelligence-workshop/data/raw/store47-2016.csv", index_col=0)
 
 print("Data has ", len(data), " entries")
 print("Column names: ", data.columns)
-print(os.getenv('MLFLOW_TRACKING_URL'))
+
 with tracking.track() as track:
         track.log_metrics(data.describe().to_dict())
