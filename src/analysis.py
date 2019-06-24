f09051eb3a4c95a6f7f8017e5425f5a4d9dff619
David Michel <robotdrdave@Davids-MacBook-Pro-3.local>
2019-06-24 15:48:09 +0100

    static output attempt at mlflow logging

--- src/analysis.py
+++ src/analysis.py
@@ -7,5 +7,6 @@ data = pd.read_csv("/Users/robotdrdave/git/continuous-intelligence-workshop/data
 
 print("Data has ", len(data), " entries")
 print("Column names: ", data.columns)
+print(os.getenv('MLFLOW_TRACKING_URL'))
 with tracking.track() as track:
         track.log_params({'param1': '0.123', 'param2': '0.456'})
