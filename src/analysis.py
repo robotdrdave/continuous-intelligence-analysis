ea520c42df84e302cbdf471cd04a87ba4466ffa5
David Michel <robotdrdave@Davids-MacBook-Pro-3.local>
2019-06-24 14:12:49 +0100

    initial repo creation

---
+++ src/analysis.py
@@ -0,0 +1,9 @@
+import tracking
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = pd.read_csv("/Users/robotdrdave/git/continuous-intelligence-workshop/data/raw/store47-2016.csv")
+
+print("Data has ", len(data), " entries")
+print("Column names: ", data.columns)
