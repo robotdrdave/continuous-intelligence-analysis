e25beb525a4ebacf9f1f312f80225dea43f28288
David Michel <robotdrdave@Davids-MacBook-Pro-3.local>
2019-06-24 16:10:49 +0100

    now logging metrics

--- src/analysis.py
+++ src/analysis.py
@@ -10,4 +10,4 @@ print("Data has ", len(data), " entries")
 print("Column names: ", data.columns)
 print(os.getenv('MLFLOW_TRACKING_URL'))
 with tracking.track() as track:
-        track.log_params({'param1': '0.123', 'param2': '0.456'})
+        track.log_metrics(data.describe().to_dict())
