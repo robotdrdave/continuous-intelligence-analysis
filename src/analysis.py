471c34193ad95475db941b1d4d4b70a4bace1fbd
David Michel <robotdrdave@Davids-MacBook-Pro-3.local>
2019-06-25 13:20:06 +0100

    mlflow wrapper removed

--- src/analysis.py
+++ src/analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import mlflow
 
 data = pd.read_csv("/Users/robotdrdave/git/continuous-intelligence-workshop/data/raw/store47-2016.csv", index_col=0)
 
@@ -15,6 +16,16 @@ plt.title('Distribution of Unit Sales')
 plt.savefig('/Users/robotdrdave/Documents/test.png')
 plt.close()
 
-with tracking.track() as track:
-        track.log_params({'metrics': str(data.describe().to_dict())})
-        track.log_graph('/Users/robotdrdave/Documents/test.png')
+#with tracking.track() as track:
+#        track.log_params({'metrics': str(data.describe().to_dict())})
+#        track.log_graph('/Users/robotdrdave/Documents/test.png')
+MLFLOW_TRACKING_URL = os.getenv('MLFLOW_TRACKING_URL')
+TENANT = os.getenv('TENANT','local')
+RUN_LABEL = os.getenv('GO_PIPELINE_LABEL', '0')
+
+mlflow.set_tracking_uri(uri=MLFLOW_TRACKING_URL)
+mlflow.set_experiment(TENANT)
+mlflow.start_run(run_name=RUN_LABEL)
+
+mlfow.log_params({'metrics': str(data.describe().to_dict())})
+mlfow.log_graph('/Users/robotdrdave/Documents/test.png')
